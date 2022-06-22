6ba05a13d6fd4e05f09b982495405b531383589f
kuldeep_daksh <kuldeep.m@teamcomputers.com>
2022-06-22 15:10:36 +0530

    Done with the AD integration

--- app/main/login.py
+++ app/main/login.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 # app.config['LDAP_BASE_DN'] = 'CN=Users,DC=tcplcoe,DC=com'
 # app.config['LDAP_USERNAME'] = 'CN=Administrator,CN=Users,DC=tcplcoe,DC=com'
 # app.config['LDAP_PASSWORD'] = 'Xanadu@@12345'
-
-username = f"CN={username},OU=Delhi AISGlass,OU=AIS Users,DC=asahiindia,DC=com"
+username = "test2"
+userfullpath = "{}"+"@asahiindia.com"
 password = "tasty@lemon09"
 
 @main.route('/login', methods=('GET', 'POST'))
@@ -24,18 +24,18 @@ def login():
     if request.method == 'POST':
         username = request.form['userid']
         password = request.form['password']
+        dn = userfullpath.format(username)
         if username and password:
             try:
-                user_exist = is_user_exist(
-                    "80.0.0.108", username, password)
-                # user_exist = is_user_exist_test(
-                #     "172.16.19.20", username, password)
-                if user_exist:
+                # ad_response = authenticate_ad_user(
+                #     "80.0.0.108", username, password)
+                ad_response = authenticate_ad_user_test(
+                    "80.0.0.108", dn, password)
+                if ad_response != "Invalid LDAP Credentials":
                     authenticate_user(username)
                     return redirect(url_for('main.index'))
                 else:
-                    error = 'Wrong email or password'
-                    return render_template("login.html", error=error)
+                    return render_template("login.html", error=ad_response)
             except Exception as e:
                 error = e
                 return render_template("login.html", error=error)
@@ -68,21 +68,24 @@ def authenticate_user(username):
     session['username'] = username
 
 
-def is_user_exist(address, username, password):
+def authenticate_ad_user(address, username, password):
     conn = ldap.initialize('ldap://' + address)
     conn.protocol_version = 3
     conn.set_option(ldap.OPT_REFERRALS, 0)
-    print("gfdknkgnhkgfnhgkhn")
-    return conn.simple_bind_s(username, password)
+    try:
+        return conn.simple_bind_s(username, password)
+    except ldap.INVALID_CREDENTIALS as e:
+        return "Invalid LDAP Credentials"
 
 
-# def is_user_exist_test(address, username, password):
-#     userid = "13666"
-#     pwd = "12345"
-#     if userid == username and password == pwd:
-#         return True
-#     else:
-#         return False
+def authenticate_ad_user_test(address, username, password):
+    print(username)
+    userid = "13666"
+    pwd = "12345"
+    if userid == username and password == pwd:
+        return True
+    else:
+        return "Invalid LDAP Credentials"
 
 
 def is_user_authenticated():
